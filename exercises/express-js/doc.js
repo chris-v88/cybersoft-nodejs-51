2e536b3562d52f10f267c0847ca31cfc9db5a934
chrv81 <vochris81@gmail.com>
2025-07-30 09:59:47 -0400

    Add documentation for extensionless, nodemon, and mysql2 in doc.js; update package.json dependencies

---
+++ exercises/express-js/doc.js
@@ -0,0 +1,9 @@
+/**
+ * extensionless: để giúp import không cần thêm đuôi js
+ * nodemon: để load lại server khi có code thay đổi
+ * mysql2: để tương tác với db (không phải là ORM)
+ *         là thư viện hỗ trợ kết nối với MySQL, MariaDB
+ *         và các cơ sở dữ liệu tương thích với MySQL.
+ *         Nó cung cấp các phương thức để thực hiện các truy vấn SQL,
+ *         quản lý kết nối và xử lý dữ liệu trả về từ cơ sở dữ liệu.
+ */
