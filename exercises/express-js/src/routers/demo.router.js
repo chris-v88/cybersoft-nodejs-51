1450532cf52152d4e23ffcd9ae161c8d7eb559c2
chrv81 <vochris81@gmail.com>
2025-07-29 22:20:44 -0400

    add root router and demo router

---
+++ exercises/express-js/src/routers/demo.router.js
@@ -0,0 +1,83 @@
+import express from 'express';
+
+const demoRouter = express.Router();
+
+/**
+ * Middleware to parse JSON bodies.
+ * This allows the server to handle JSON data sent in requests.
+ * @param {string} path - The path to the JSON body parser
+ * @param {Function} callback - The callback function to execute when the middleware is routerlied
+ */
+
+// resquest: The request object containing information about the HTTP request
+// response: The response object used to send a response back to the client
+// next: The next middleware function in the stack
+demoRouter.get('/login', (request, response, next) => {
+  response.json('hello world');
+});
+
+// ví dụ trong postman:
+// GET http://localhost:3069/query?email=tu@email.com&name=tu
+/**
+ * Nhận dữ liệu ở quẻy
+ * - nhận biết: sau dáu ? và mỗi tham số cách nhau bằng dấu &
+ * - thường dùng cho: phân trang, lọc dữ liệu, tìm kiếm
+ * - tránh: thiế kế nhận qua body vì FE (axios) chặn gủiw body với GET
+ */
+demoRouter.get('/query', (request, response, next) => {
+  const query = request.query;
+
+  response.json({
+    message: 'Query received',
+    query: query,
+  });
+});
+
+/**
+ * Path parameters
+ * - Nhận biết: được xác định bằng dấu hai chấm : trong url
+ * - thường dùng: (lấy, xoá, cập nhật) một phần tử cụ thể thông qua id
+ * - id : nếu là số, sẽ là chuỗi số (sẽ luôn luôn là CHUỖI)
+ */
+demoRouter.put('/path/:id', (request, response, next) => {
+  const param = request.params;
+
+  console.log('Path parameter:', param);
+
+  // Thực hiện cập nhật dữ liệu
+  response.json({
+    message: 'Data updated successfully',
+    data: null,
+  });
+});
+
+/**
+ * Headers
+ * Thường dùng: xác thực token (authentization), meta dât, x-api-key
+ *
+ */
+demoRouter.delete('/delete', (request, response, next) => {
+  const headers = request.headers;
+
+  console.log('Headers:', headers);
+
+  // Thực hiện xóa dữ liệu
+  response.json({
+    message: 'Data deleted successfully',
+    data: headers,
+  });
+});
+
+/**
+ * Body
+ */
+demoRouter.post('/body', (request, response, next) => {
+  const body = request.body;
+
+  response.json({
+    message: 'Body received',
+    body: body,
+  });
+});
+
+export default demoRouter;
