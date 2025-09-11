05cab14990dc7d80b600168d586af5616d73598e
chrv81 <vochris81@gmail.com>
2025-09-10 20:59:04 -0400

    add dto

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ArticleModule } from './modules-api/article/article.module';
 import { PrismaModule } from './modules-system/prisma/prisma.module';
 import { ConfigModule } from './modules-system/config/config.module'; // Import ConfigModule để quản lý các biến môi trường
+import { AuthModule } from './modules/modules-api/auth/auth.module';
 
 @Module({
   // gắn config module vào đây trong import
@@ -11,6 +12,7 @@ import { ConfigModule } from './modules-system/config/config.module'; // Import 
     ConfigModule, // Thêm ConfigModule vào đây để toàn bộ app có thể sử dụng các biến môi trường
     ArticleModule,
     PrismaModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
